4f966b6993c267028ad844af26f8d0b6db3ae4b6
alcho01 <74156727+alcho01@users.noreply.github.com>
2023-02-11 21:22:26 -0500

    Prototype1 - Added pressure plate which opens door

--- Assets/scripts/PlayerInteraction.cs
+++ Assets/scripts/PlayerInteraction.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInteraction : MonoBehaviour
 {
+    [SerializeField] Text LevelCompleteText;
 
-    private void Update() {
+    void Start()
+    {
+        LevelCompleteText.gameObject.SetActive(false);
+    }
+
+    void Update() {
         if (Input.GetKeyDown(KeyCode.E)) {
             float interactRange = 2f;
             Collider[] colliderArray = Physics.OverlapSphere (transform.position, interactRange);
             foreach(Collider collider in colliderArray) {
                 if (collider.TryGetComponent(out StoveInteraction stoveInteraction)) {
                     stoveInteraction.Interact();
+
+                    LevelComplete();
+                    Time.timeScale = 0f;
                 }
+
+               
             }
         }
+    }
+
+    public void LevelComplete()
+    {
+        LevelCompleteText.gameObject.SetActive(true);
     }
 }
