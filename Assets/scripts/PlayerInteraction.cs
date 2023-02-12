e5dd061876f9647dcefaced7972edc12e805bd51
alcho01 <74156727+alcho01@users.noreply.github.com>
2023-02-12 14:35:44 -0500

    Prototype 1 Added finishing touches

--- Assets/scripts/PlayerInteraction.cs
+++ Assets/scripts/PlayerInteraction.cs
@@ -17,6 +17,14 @@ public class PlayerInteraction : MonoBehaviour
             float interactRange = 2f;
             Collider[] colliderArray = Physics.OverlapSphere (transform.position, interactRange);
             foreach(Collider collider in colliderArray) {
+
+                if (collider.TryGetComponent(out FridgeInteraction fridgeInteraction))
+                {
+
+          
+                    fridgeInteraction.Interact();
+                }
+
                 if (collider.TryGetComponent(out StoveInteraction stoveInteraction)) {
                     stoveInteraction.Interact();
 
